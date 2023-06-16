39dbb935ca0a35e0f338e2bd4b747ce8c186daa3
N0N0suke <N0N0suke@users.noreply.github.com>
2023-06-16 04:28:56 +0000

    deploy: 6e54b1ad545c560dcce9f1b7f36e9b7da46e1cf5

---
+++ _next/static/SmQY9AzFLpvlsehGNCHFB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/89bd8bb59a96520f.css","static/chunks/pages/index-376cf21db6cb42c3.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
